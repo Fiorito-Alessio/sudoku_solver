e2c5fcaee4b00c858f7aa093b5812a910c5be090
Alessio Fiorito <alessio.fiorito@outlook.com>
2025-12-02 09:56:28 +0100

    main completed and program finished

--- src/main.c
+++ src/main.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "grid.h"
+#include "solver.h"
 
 
 int main(void) {
-
     int *grid = create_grid();
+    printf("Initial grid :\n");
     display_grid(grid);
+
+    if (solve(grid)) {
+        printf("\nSudoku completed successfully !\n");
+        display_grid(grid);
+    } else {
+        printf("\nSudoku is impossible.\n");
+    }
 
     return EXIT_SUCCESS;
 }
